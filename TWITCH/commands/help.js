3da8bc6f6938e240761603fe207c269ef7e8cc43
vertikarl <rayquasa181@gmail.com>
2020-06-20 20:33:50 +0200

    updated help for alleshusos.de V2

--- TWITCH/commands/help.js
+++ TWITCH/commands/help.js
@@ -4,11 +4,10 @@ module.exports = {
     description: 'sends the link to this help form',
     execute(chatClient,channel,user,message,args) {
         user = user.displayName
-        F.writeFile('./help', '', function() {})
+        F.writeFile('../alleshusos.de/public/commands.csv', '', function() {})
         chatClient.huso.forEach((key, command) => {
-            F.appendFile('./help', `${process.env.prefix}${command}\n${key.description}\n`, function() {})
-            F.appendFile('./help', `------------\n`, function() {})
+            F.appendFile('../alleshusos.de/public/commands.csv', `"${process.env.prefix}${command}","${key.description}"\n`, function() {})
         })
-        chatClient.action(channel, 'Here is a list of commands: https://alleshusos.de/HahnrichJS/help')
+        chatClient.action(channel, 'Here is a list of commands: https://alleshusos.de/commands')
     }
 }
