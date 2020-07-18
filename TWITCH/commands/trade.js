f985f017b9b218784d5db84e17620d96ee57c201
dogr95 <rayquasa181@gmail.com>
2020-07-18 16:08:08 +0200

    fixed trade

--- TWITCH/commands/trade.js
+++ TWITCH/commands/trade.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'trade',
     description: 'times you out for 100 seconds',
-    execute(chatClient, channel, user, message, args, TwitchClient, check_watchtime, active_users) {
-        chatClient.timeout(channel, user, 100, 'no u')
+    execute(chatClient,channel,user,message,args,TwitchClient,check_watchtime,active_users,load_db,xp_and_level) {
+        chatClient.timeout(channel, user.displayName, 100, 'no u').then(res => console.log(res))
         }
 }
