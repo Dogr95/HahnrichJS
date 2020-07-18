cb9a1ab80f3fac0639990bb460ddd80293f22ed8
dogr95 <rayquasa181@gmail.com>
2020-07-18 16:11:31 +0200

    ~

--- TWITCH/commands/profile.js
+++ TWITCH/commands/profile.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'trade',
+    name: 'profile',
     description: 'times you out for 100 seconds',
     execute(chatClient,channel,user,message,args,TwitchClient,check_watchtime,active_users,load_db,xp_and_level) {
         chatClient.timeout(channel, user.displayName, 100, 'no u').then(res => console.log(res))
