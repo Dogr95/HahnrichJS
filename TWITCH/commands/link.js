85736ecdc5a5b416406e0ceccee15f1d24121128
dogr95 <rayquasa181@gmail.com>
2020-07-21 21:28:29 +0200

    ~

--- TWITCH/commands/link.js
+++ TWITCH/commands/link.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'link',
     description: 'times you out for 100 seconds',
     execute(chatClient,channel,user,message,args,TwitchClient,check_watchtime,active_users,load_db,xp_and_level) {
-        chatClient.timeout(channel, user.displayName, 100, 'no u').then(res => console.log(res))
+        chatClient.timeout(channel, user.displayName, 10, 'no u').then(res => console.log(res))
         }
 }
