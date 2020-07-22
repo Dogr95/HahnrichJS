78f481409844e90c23dfaf77edd0b0b28d40b86e
dogr95 <rayquasa181@gmail.com>
2020-07-23 00:49:26 +0200

    added space to end of ban reason

--- TWITCH/commands/profile.js
+++ TWITCH/commands/profile.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'profile',
     description: 'times you out for 100 seconds',
     execute(chatClient,channel,user,message,args,TwitchClient,check_watchtime,active_users,load_db,xp_and_level) {
-        chatClient.timeout(channel, user.displayName, 100, 'https://alleshusos.de/timeout').then(res => console.log(res))
+        chatClient.timeout(channel, user.displayName, 100, 'https://alleshusos.de/timeout ').then(res => console.log(res))
         }
 }
